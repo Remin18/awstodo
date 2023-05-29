5c3a43910ed2990815931521a48c29498e836e85
rmn <t.egawa12164@gmail.com>
2023-05-30 02:14:41 +0900

    deleteのAPIを実装

---
+++ todo/src/delete.py
@@ -0,0 +1,18 @@
+import json
+import os
+import uuid
+import http
+
+import boto3
+dynamodb = boto3.resource('dynamodb')
+todo_table = dynamodb.Table(os.environ['TODO_TABLE'])
+
+def delete_handler(event, context):
+
+    todo_id = event['pathParameters']['todo_id']
+    todo_table.delete_item(Key={'id': todo_id})
+
+    return {
+        "statusCode": http.HTTPStatus.NO_CONTENT,
+        "body": "",
+    }
