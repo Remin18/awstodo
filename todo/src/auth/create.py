626f4602021e0ec2fd90d9ccf4a6b260b19cf807
rmn <t.egawa12164@gmail.com>
2023-06-11 04:24:55 +0900

    userをauthにリネーム

---
+++ todo/src/auth/create.py
@@ -0,0 +1,32 @@
+import boto3
+import os
+import http
+import json
+
+client_id = os.environ['CLIENT_ID']
+region = os.environ['REGION']
+client = boto3.client('cognito-idp', region_name=region)
+
+def create_user_handler(event, context):
+
+    body = json.loads(event['body'])
+    username = body['username']
+    password = body['password']
+
+    try:
+        response = client.sign_up(
+            ClientId=client_id,
+            Username=username,
+            Password=password
+        )
+
+        return {
+            'statusCode': http.HTTPStatus.OK,
+            'body': 'user create successfully'
+        }
+    except Exception as e:
+        print(str(e))
+        return {
+            'statusCode': http.HTTPStatus.INTERNAL_SERVER_ERROR,
+            'body': 'An error occurred while creating user'
+        }
